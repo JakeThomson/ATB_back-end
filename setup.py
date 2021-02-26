af083f2803eb9c441a0bb3893820c549e34d4c87
JakeThomson <jakethomson0@gmail.com>
2021-02-26 18:34:56 +0000

    Added setup.py and README.md

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Algorithmic Trading Bot",
+    version="0.0.6",
+    author="Jake Thomson",
+    author_email="jakethomson0@gmail.com",
+    description="The backend of the algorithmic trading bot project.",
+    long_description="long_description",
+    long_description_content_type="text/markdown",
+    url="https://github.com/JakeThomson/ATB_back-end",
+    project_urls={
+        "UI": "https://diss.jake-t.codes",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+)
