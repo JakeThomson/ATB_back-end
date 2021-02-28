1389f2a5005287751468e528f2933535fc725e19
JakeThomson <jakethomson0@gmail.com>
2021-02-28 18:52:18 +0000

    Added test to check if  get_tickers updates the ticker cache list.

--- tests/data_handlers/test_historical_data_handler.py
+++ tests/data_handlers/test_historical_data_handler.py
@@ -1,7 +1,7 @@
 import pytest
 import requests
 import datetime as dt
-import os.path
+import os
 from data_handlers.historical_data_handler import HistoricalDataManager
 
 hist_data_mgr = HistoricalDataManager(market_index="S&P500")
@@ -21,8 +21,27 @@ def test_get_tickers_extracts_values_from_html_table(requests_mock):
 
 @pytest.mark.historical_data_handler
 def test_get_tickers_creates_ticker_list_cache_file():
+    # Check to see if cache file was created.
     today_date = dt.date.today()
     file_exists = os.path.isfile(f"data_handlers/historical_data/market_index_lists/S&P500_{today_date}.pickle")
 
     assert file_exists
 
+
+@pytest.mark.historical_data_handler
+def test_get_tickers_updates_ticker_list_cache_file(requests_mock):
+    # Test setup: Mock wikipedia endpoint, and change the previously created cache file to yesterday's date.
+    with open('data_handlers/test_data/test_wikipedia_html.txt') as f:
+        text = f.read()
+    requests_mock.get("https://en.wikipedia.org/wiki/List_of_S%26P_500_companies", text=text)
+    today_date = dt.date.today()
+    yesterday_date = dt.date.today() - dt.timedelta(days=1)
+    os.rename(fr'data_handlers/historical_data/market_index_lists/S&P500_{today_date}.pickle',
+              fr'data_handlers/historical_data/market_index_lists/S&P500_{yesterday_date}.pickle')
+
+    # Test to see if grab_tickers updates the file.
+    hist_data_mgr.grab_tickers()
+
+    file_exists = os.path.isfile(f"data_handlers/historical_data/market_index_lists/S&P500_{today_date}.pickle")
+
+    assert file_exists
