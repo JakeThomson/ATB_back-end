b0061d130a28b5fda778ec463af2e3c9fcda2fbb
JakeThomson <jakethomson0@gmail.com>
2021-02-28 16:41:53 +0000

    Made test case generator more generic.

--- tests/data_validators/test_historical_data_validator.py
+++ tests/data_validators/test_historical_data_validator.py
@@ -12,32 +12,31 @@ class DataCases:
     """
     case_path = "data_validators/cases/historical_data_cases/"
     cases = listdir(case_path)
+    case_ids = []
 
     # Set up dataframes to be used within historical_data_validator.
     for i, case_name in enumerate(cases):
         case = pd.read_csv(case_path + case_name)
         case['Date'] = pd.to_datetime(case['Date'])
         case = case.set_index("Date")
-        pattern = re.compile("(valid|invalid)_historical_data_(.*).csv")
+        pattern = re.compile("((valid|invalid).*).csv")
         match = pattern.match(case_name)
-        case.valid = match.group(1)
-        case.label = match.group(2)
+        case.id = match.group(1)
+        case.valid = match.group(2)
+        case_ids.append(match.group(1))
         case.ticker = "TEST"
         cases[i] = case
 
-    # Extract invalid and valid cases relevant to the date_gap test.
-    date_gap_cases = [case for case in cases if case.label == "date_gap" or case.valid == "valid"]
-
-    @parametrize(case=("valid", "invalid"))
-    def data_gap_select(self, case):
+    @parametrize(case=case_ids)
+    def case_select(self, case):
         """ Returns a row for each case to be used by the tests. """
-        for dataframe in self.date_gap_cases:
-            if dataframe.valid == case:
-                return dataframe
+        for case_df in self.cases:
+            if case_df.id == case:
+                return case_df
 
 
 @pytest.mark.historical_data_validator
-@parametrize_with_cases("case", cases=DataCases, prefix="data_gap_")
+@parametrize_with_cases("case", cases=DataCases)
 def test_date_gap(case):
     validator = historical_data_validator.HistoricalDataValidator(case, max_day_gap=5)
     expected = True if case.valid == "valid" else False
